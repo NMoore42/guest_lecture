12399c940499d134eef43de1ef0b0344a5f44864
NMoore42 <nicholas.moore5@icloud.com>
2020-08-11 19:43:23 -0500

    complete readme

---
+++ operators.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Operator
+
+  def unary_example
+    #Insert unary code here
+  end
+
+  def binary_example
+    #Insert binary code here
+  end
+
+  def ternary_example
+    #Insert ternary code here
+  end
+
+end
+
+#Operator Instances
+test = Operator.new
+
+binding.pry
+0
